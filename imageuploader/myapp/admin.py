f0273167a6167fa4b8d51549557b635c617ac337
yuvashyam <yuvashyam1@gmail.com>
2021-10-05 18:55:45 +0530

    first

---
+++ imageuploader/myapp/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Image
+
+# Register your models here.
+class ImageAdmin(admin.ModelAdmin):
+    fields=['id','photo','date']
+    #list_display=['id','photo','date']
+
+admin.site.register(Image)
+
